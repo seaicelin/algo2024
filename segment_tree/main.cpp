4955188a8533adc156a133a58681a0aaf6369034
seaice <339533005@qq.com>
2024-03-10 22:49:36 +0800

    segment tree

---
+++ segment_tree/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Node {
+    int left;
+    int right;
+    int sum;
+    int add;
+    Node(): left(0), right(0),sum(0),add(0){}
+};
+
+const static int SIZE = 10;
+
+void buildSegmentTree();
+void searchSegmentTree();
+void updateSegmentTree();
+
+
+void buildSegmentTree(){
+
+}
+void searchSegmentTree(){}
+void updateSegmentTree(){}
+
+int main() {
+
+    buildSegmentTree();
+    searchSegmentTree();
+    updateSegmentTree();
+
+    return 0;
+}
