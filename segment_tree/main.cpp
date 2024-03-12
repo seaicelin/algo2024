0b5543e13f878463f4562e2ada2512ab11c6942b
seaice <339533005@qq.com>
2024-03-13 00:29:45 +0800

    seg_tree

--- segment_tree/main.cpp
+++ segment_tree/main.cpp
@@ -14,8 +14,10 @@ Node seg_tree[4*SIZE];
 
 void buildSegmentTree(int arr[], int left, int right, int pos);
 int searchSegmentTree(int left, int right, int pos);
-void updateSegmentTree();
-
+void updateSegmentTree(int left, int right, int add, int pos);
+void update(int pos, int x, int k);
+int query(int pos, int x, int y);
+void update(int pos, int x, int y, int k);
 
 void buildSegmentTree(int arr[], int left, int right, int pos){
     if (pos < 0 || pos >= 4*SIZE) {
@@ -60,7 +62,56 @@ int searchSegmentTree(int left, int right, int pos) {
     return ret;
 }
 
-void updateSegmentTree(){}
+int query(int pos, int x, int y) {
+    if (seg_tree[pos].left >= x && seg_tree[pos].right <= y) {
+        return seg_tree[pos].sum;
+    }
+
+    int lson = pos * 2 + 1;
+    int rson = pos * 2 + 2;
+    int mid = (seg_tree[pos].left + seg_tree[pos].right) / 2;
+
+    int sum = 0;
+    if (x <= mid) {
+        sum += query(lson, x, y);
+    }
+    if (y > mid) {
+        sum += query(rson, x, y);
+    }
+
+    return sum;
+}
+
+void test_query(int arr[]) {
+    for (int i = 0; i < SIZE; i++) {
+        int sum = arr[i];
+        for (int j = i + 1; j < SIZE; j++) {
+            sum += arr[j];
+            if (sum != query(0, i, j)) {
+                printf("test_query error \n");
+                return;
+            }
+        }
+    }
+    printf("test_query success!\n");
+}
+
+void updateSegmentTree(int left, int right, int add, int pos) {
+    if (left <= seg_tree[pos].left && right >= seg_tree[pos].right) {
+        seg_tree[pos].add = add;
+        return;
+    }
+
+    int lson = pos * 2 + 1;
+    int rson = pos * 2 + 2;
+    int ret = 0;
+    if (seg_tree[lson].right >= left) {
+        ret += searchSegmentTree(left, right, lson);
+    }
+    if (seg_tree[rson].left <= right) {
+        ret += searchSegmentTree(left, right, rson);
+    }
+}
 
 void test_build_tree(int arr[]) {
     int index = 0;
@@ -72,13 +123,98 @@ void test_build_tree(int arr[]) {
         if (seg_tree[index].sum == sum) {
             printf("[%d, %d] = %d\n", seg_tree[index].left, seg_tree[index].right, sum);
         } else {
-            printf("error \n");
-            while(1);
+            printf("test_build_tree error \n");
+            return;
+            //while(1);
         }
         index++;
     }
 }
 
+
+
+void test_search_seg_tree(int arr[]) {
+    for (int i = 0; i < SIZE; i++) {
+        int sum = arr[i];
+        for (int j = i; j < SIZE; j++) {
+            if (i != j) {
+                sum += arr[j];
+            }
+            if (sum != searchSegmentTree(i, j, 0)) {
+                printf("test_search_seg_tree i=%d, j = %d sum = %d error \n",i,j,sum);
+                return;
+            }
+        }
+    }
+    printf("test_search_seg_tree success!\n");
+}
+
+void update(int pos, int x, int k) {
+    if (seg_tree[pos].left == x && seg_tree[pos].right == x) {
+        seg_tree[pos].sum += k;
+        return;
+    }
+    int lson = pos * 2 + 1;
+    int rson = pos * 2 + 2;
+    int mid = (seg_tree[pos].left + seg_tree[pos].right) / 2;
+    if (x <= mid) {
+        update(lson, x, k);
+    }
+    if (x > mid) {
+        update(rson, x, k);
+    }
+    seg_tree[pos].sum = seg_tree[lson].sum + seg_tree[rson].sum;
+}
+
+void test_update(int arr[]) {
+    for (int i = 0; i < SIZE; i++) {
+        int sum = arr[i] += 5;
+        update(0, i, 5);
+        for (int j = i + 1; j < SIZE; j++) {
+            sum += arr[j];
+            if (sum != query(0, i, j)) {
+                printf("test_udpate error \n");
+                return;
+            }
+        }
+    }
+    printf("test_udpate success!\n");
+}
+
+void pushdown(int pos) {
+    if (seg_tree[pos].add) {
+        int lson = pos*2 + 1;
+        int rson = pos*2 + 2;
+        seg_tree[lson].sum += seg_tree[pos].add * (seg_tree[lson].right - seg_tree[lson].left + 1);
+        seg_tree[rson].sum += seg_tree[pos].add * (seg_tree[rson].right - seg_tree[lson].left + 1);
+        seg_tree[lson].add += seg_tree[pos].add;
+        seg_tree[rson].add += seg_tree[pos].add;
+        seg_tree[pos].add = 0;
+    }
+}
+
+void pushup(int pos) {
+    seg_tree[pos].sum = seg_tree[2*pos+1].sum + seg_tree[2*pos+2].sum;
+}
+
+void update(int pos, int x, int y, int k) {
+    if(seg_tree[pos].left >= x && seg_tree[pos].right <= y) {
+        seg_tree[pos].sum += (seg_tree[pos].right - seg_tree[pos].left + 1) * k;
+        seg_tree[pos].add += k;
+        return;
+    }
+    int lson = pos*2 + 1;
+    int rson = pos*2 + 1;
+    int mid = seg_tree[pos].left + seg_tree[pos].right >> 1;
+    pushdown(pos);
+    if (x <= mid) {
+        update(lson, x, y, k);
+    }
+    if (y > mid) {
+        update(rson, x, y, k);
+    }
+    pushup(pos);
+}
 
 int main() {
 
@@ -87,10 +223,13 @@ int main() {
     buildSegmentTree(arr, 0, SIZE - 1, 0);
     test_build_tree(arr);
     
-    int ret = searchSegmentTree(0, 8, 0);
-    printf("ret = %d", ret);
+    //int ret = searchSegmentTree(0, 8, 0);
+    //printf("ret = %d\n", ret);
+    test_search_seg_tree(arr);
+    test_query(arr);
+    test_update(arr);
 
-    updateSegmentTree();
+    //updateSegmentTree(0, 5, 5);
 
     return 0;
 }
