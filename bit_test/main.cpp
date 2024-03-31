0079dad57ede60121abfcc43f9f69f3f49c1f226
seaice <339533005@qq.com>
2024-03-31 18:15:24 +0800

    push

--- bit_test/main.cpp
+++ bit_test/main.cpp
@@ -5,9 +5,11 @@
 int getRand(int min, int max);
 int putStep(long long state, int step, long long* p);
 int getStep(long long state, int& step, long long* p);
+int my_hash(long long state);
 
 #define SIZE 362880
 #define VIS_SIZE 9
+#define mod 1000007
 //123456780 全排列 9！
 
 struct StateNode{
@@ -21,12 +23,13 @@ int stateListLen = 0;
 struct MapNode {
     long long state;
     int step;
-    long long* pStart;
-    MapNode():state(0),step(0), pStart(nullptr){}
+    int index;
+    MapNode():state(0),step(0), index(-1){}
 };
 MapNode stepMap[SIZE];
 
-
+long long storeMode[SIZE];
+int storeModeLen = 0;
 
 
 
@@ -38,8 +41,9 @@ void initVis() {
 }
 void getAllState(int sum, int n) {
     if (n == VIS_SIZE) {
-        stateList[stateListLen++].state = sum;
-        stateList[stateListLen++].mode = getRand(0, 3);
+        stateList[stateListLen].state = sum;
+        stateList[stateListLen].mode = getRand(0, 3);
+        stateListLen++;
         return;
     }
 
@@ -62,12 +66,41 @@ int getRand(int min, int max) {
     return (rand() % (max - min + 1) + min);
 }
 
-int putStep(long long state, int step, long long* p) {
-
+int my_hash(long long state) {
+    return state % mod;
 }
 
-int getStep(long long state, int& step, long long* p) {
-    
+int putStep(long long state, int step, int index) {
+    if(step == 0) {
+        return -1;
+    }
+    int key = my_hash(state);
+    while(stepMap[key].step != 0) {
+        if (stepMap[key].state == state) {
+            return -1;
+        }
+        key++;
+        key %= SIZE;
+    }
+
+    stepMap[key].state = state;
+    stepMap[key].step = step;
+    stepMap[key].index = index;
+    return 0;
+}
+
+int getStep(long long state, int& step, int& index) {
+    int key = my_hash(state);
+    while(stepMap[key].step != 0) {
+        if (stepMap[key].state == state) {
+            step = stepMap[key].step;
+            index = stepMap[key].index;
+            return 0;
+        }
+        key++;
+        key %= SIZE;
+    }
+    return -1;
 }
 
 int main() {
@@ -77,9 +110,78 @@ int main() {
     printf("%lld,%d\n", stateList[0].state, stateListLen);
     printf("%lld,%d\n", stateList[stateListLen-1].state, stateListLen);
 
-    for (int i = 0; i < 100; i++) {
-        
+
+    for (int j = 0; j < 100; j++) {
+        int storeModeIndex = storeModeLen;//&storeMode[storeModeLen++];
+        int stepCount = 0;
+        long long* pMode = &storeMode[storeModeLen++];
+        int step = 0;
+        for (int i = j; i < 100; i++) {
+            long long curMode = stateList[i].mode;
+            //printf("curMode=%d\n", curMode);
+            *pMode = (*pMode) | (curMode << (i << 1));
+            step++;
+            //printf("i=%d,curMode=%lld,storeMode=%lld,stepCont=%d\n", i, curMode, (((*pMode) >> (i << 1)) & 0x3), stepCount);
+            stepCount++;
+            if (stepCount == 32) {
+                stepCount = 0;
+                pMode = &storeMode[storeModeLen++];
+            }
+        }
+        int ret = putStep(stateList[j].state, step, storeModeIndex);
+        printf("putStep j=%d, ret = %d, pStart=%d\n", j, ret, storeModeIndex);
     }
+
+    for (int j = 0; j < 100; j++) {
+        int stepCount = 0;
+        int step;
+        int index;
+        int ret = getStep(stateList[j].state, step, index);
+        printf("getStep ret = %d, pstart=%d\n", ret, index);
+        long long* pStart = &storeMode[index];
+        for (int i = j; i < 100; i++) {
+            int mode = stateList[i].mode;
+            int storeMode = (((*pStart) >> (i << 1)) & 0x3);
+            if (mode != storeMode) {
+                printf("fail\n");
+            } else {
+                printf("success\n");
+            }
+            stepCount++;
+            if (stepCount == 32) {
+                stepCount = 0;
+                pStart++;
+            }
+        }
+    }
+
+    for (int j = 0; j < 100; j++) {
+        int stepCount = 0;
+        int step;
+        int index;
+        int ret = getStep(stateList[j].state, step, index);
+        printf("getStep ret = %d, pstart=%d\n", ret, index);
+        long long* pStart = &storeMode[index];
+        int i = j;
+        while(step--) {
+            int mode = stateList[i].mode;
+            int storeMode = (((*pStart) >> (i << 1)) & 0x3);
+            i++;
+            if (mode != storeMode) {
+                printf("fail\n");
+            } else {
+                printf("success\n");
+            }
+            stepCount++;
+            if (stepCount == 32) {
+                stepCount = 0;
+                pStart++;
+            }
+        }
+    }    
+
+
+
 
     return 0;
 }
---
+++ sec_main/huakuaipintu/sec_main.h
@@ -0,0 +1,5 @@
+
+#define SIZE 3
+
+void test(const int puzzle[SIZE][SIZE]);
+void move(int mode);
